a897596da942b16e120a693e4ae17e5ed078e772
zsxoff <dobratulin@yahoo.com>
2021-01-12 21:39:29 +0300

    update fonts

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo dnf install -y google-croscore-* google-noto-sans-fonts
+sudo dnf install -y \
+    google-arimo-fonts \
+    google-cousine-fonts \
+    google-noto-sans-fonts \
+    google-noto-sans-mono-fonts \
+    google-roboto-condensed-fonts \
+    google-roboto-fonts \
+    google-roboto-mono-fonts \
+    google-roboto-slab-fonts \
+    google-tinos-fonts
